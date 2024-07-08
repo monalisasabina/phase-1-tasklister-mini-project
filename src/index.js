97a71514a8ce1fb608def779303a4e2ba07fd5aa
Monalisa Onyango <+>
2024-07-08 22:43:11 +0300

    new task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+   let form =document.querySelector('form')
+       form.addEventListener('submit',(event) => {
+       event.preventDefault();
+       puttingTheList(event.target.querySelector('#new-task-description').value)
+       form.reset()
+   }) 
 });
+
+function puttingTheList(newTask){
+  let li =document.createElement('li')
+  li.textContent= `${newTask}`
+  document.querySelector('#tasks').appendChild(li)
+
+  // adding button to the list
+  let btn =document.createElement('button');
+  btn.textContent='x'
+  document.querySelector('#tasks').appendChild(btn)
+
+  //delete the list when clicked using the button
+  btn.addEventListener('click',deleteButton)
+}
+
+function deleteButton(event){
+  event.target.parentNode.remove()
+}
